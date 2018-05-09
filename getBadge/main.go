db7e2a07109ecfba6e1154102e0ccbed04c7737b
Joshua Walton <Josh.Walton@disputeresolutiondata.com>
2018-05-09 11:15:58 -0700

    good stopping point...still can't seem to get envs back, although in correct region context

--- getBadge/main.go
+++ getBadge/main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-        "github.com/aws/aws-lambda-go/lambda"
+    "fmt"
+
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+
 )
 
-type Response struct {
-        Message string `json:"message"`
-}
 
-func Handler() (Response, error) {
-        return Response{
-                Message: "Go Serverless v1.0! Your function executed successfully!",
-        }, nil
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+  sess := session.New()
+  fmt.Println("region is: ", *sess.Config.Region)
+  svc := elasticbeanstalk.New(sess)
+  input := &elasticbeanstalk.DescribeEnvironmentsInput{
+      EnvironmentIds: []*string{
+          aws.String(request.PathParameters["environmentId"]),
+      },
+  }
+  fmt.Println("Received Input: ", input)
+  result, err := svc.DescribeEnvironments(input)
+  if err != nil {
+      if aerr, ok := err.(awserr.Error); ok {
+          switch aerr.Code() {
+          default:
+              fmt.Println(aerr.Error())
+          }
+      } else {
+          // Print the error, cast err to awserr.Error to get the Code and
+          // Message from an error.
+          fmt.Println(err.Error())
+      }
+      return events.APIGatewayProxyResponse{Body:  "that didnt work for now", StatusCode: 200}, nil
+  }
+
+  fmt.Println("result is:", result)
+	return events.APIGatewayProxyResponse{Body:  "thatll work for now", StatusCode: 200}, nil
 }
 
 func main() {
-        lambda.Start(Handler)
+	lambda.Start(Handler)
 }
