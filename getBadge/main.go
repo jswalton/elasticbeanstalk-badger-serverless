bf61d1f25c605dad068d4948e20707fa787fbd97
Joshua Walton <jswalton21@gmail.com>
2018-05-10 13:45:52 -0700

    its alive! (but not with the best code in the world. im def not good at golang

--- getBadge/main.go
+++ getBadge/main.go
@@ -4,49 +4,73 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "strings"
 
     "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
-    // "github.com/aws/aws-sdk-go/service/elasticbeanstalk"
-    // "github.com/aws/aws-sdk-go/aws/session"
-    // "github.com/aws/aws-sdk-go/aws"
-    // "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
 
 )
 
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-  // sess := session.New()
-  // fmt.Println("region is: ", *sess.Config.Region)
-  // svc := elasticbeanstalk.New(sess)
-  // input := &elasticbeanstalk.DescribeEnvironmentsInput{
-  //     EnvironmentIds: []*string{
-  //         aws.String(request.PathParameters["environmentId"]),
-  //     },
-  // }
-  // fmt.Println("Received Input: ", input)
-  // result, err := svc.DescribeEnvironments(input)
-  // if err != nil {
-  //     if aerr, ok := err.(awserr.Error); ok {
-  //         switch aerr.Code() {
-  //         default:
-  //             fmt.Println(aerr.Error())
-  //         }
-  //     } else {
-  //         // Print the error, cast err to awserr.Error to get the Code and
-  //         // Message from an error.
-  //         fmt.Println(err.Error())
-  //     }
-  //
-  //     return events.APIGatewayProxyResponse{Body: "Error describing env", StatusCode: 400}, nil
-  // }
-  // // envName := result.environments[0].EnvironmentName //.gsub! '-', '_'
-	// // envStatus := result.environments[0].Status //.downcase.capitalize
-	// // envHealth := result.environments[0].Health //.downcase
-	// // envAppVersion := result.environments[0].VersionLabel[0,10]
+  //create new session for ElasticBeanstalk SDK and get environment descriptions for requested env
+  sess := session.New()
+  fmt.Println("region is: ", *sess.Config.Region)
+  svc := elasticbeanstalk.New(sess)
+  input := &elasticbeanstalk.DescribeEnvironmentsInput{
+      EnvironmentIds: []*string{
+          aws.String(request.PathParameters["environmentId"]),
+      },
+  }
+  //check the result for errors
+  result, err := svc.DescribeEnvironments(input)
+  if err != nil {
+      if aerr, ok := err.(awserr.Error); ok {
+          switch aerr.Code() {
+          default:
+              fmt.Println(aerr.Error())
+          }
+      } else {
+          // Print the error, cast err to awserr.Error to get the Code and
+          // Message from an error.
+          fmt.Println(err.Error())
+      }
 
-  url := fmt.Sprintf("https://img.shields.io/badge/bloop-bleep_Version_292-green.svg")
+      return events.APIGatewayProxyResponse{Body: "Error describing env", StatusCode: 400}, nil
+  }
+
+  //defaults for EB env variables
+  envName := "<nil>"
+  envStatus := "<nil>"
+  envHealth := "<nil>"
+  envAppVersion := "<nil>"
+
+  //if there are environments returned, go ahead and assign the information to our EB env variables
+  if len(result.Environments) > 0 {
+    if result.Environments[0].EnvironmentName  != nil { // Check for nil!
+      envName = strings.Replace(*result.Environments[0].EnvironmentName, "-", "_", -1)
+    }
+    if result.Environments[0].Status  != nil { // Check for nil!
+      envStatus = *result.Environments[0].Status
+    }
+    if result.Environments[0].Health  != nil { // Check for nil!
+      envHealth = strings.ToLower(*result.Environments[0].Health)
+    }
+    if result.Environments[0].VersionLabel  != nil { // Check for nil!
+      envAppVersion = *result.Environments[0].VersionLabel
+    }
+  } else {
+    return events.APIGatewayProxyResponse{Body: "No Environment Found", StatusCode: 400}, nil
+  }
+
+  //Go get the badge from img.shields.io
+  url := fmt.Sprintf("http://img.shields.io/badge/" + envName + "-" +envStatus+ "_Version_" + envAppVersion + "-" + envHealth + ".svg")
+  fmt.Println("url will be: ", url)
 
   client := &http.Client{}
 
@@ -55,18 +79,23 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
     return events.APIGatewayProxyResponse{Body: "Error getting badge for env", StatusCode: 400}, nil
   }
 
-
+  //check the img.shields.io response
   resp, err := client.Do(req)
   if err != nil {
     return events.APIGatewayProxyResponse{Body: "Error getting badge for env", StatusCode: 400}, nil
   }
   defer resp.Body.Close()
 
+  //get the body bytes (svg) to be returned
   bodyBytes, err2 := ioutil.ReadAll(resp.Body)
   if err2 != nil {
     return events.APIGatewayProxyResponse{Body: "Error getting badge for env", StatusCode: 400}, nil
   }
+
+  //create headers for content type
   headers := map[string]string{ "content-type": "image/svg+xml"}
+
+  //return the APIGatewayProxyRequest with the svg badge image as the body
 	return events.APIGatewayProxyResponse{Body: string(bodyBytes), StatusCode: 200, Headers: headers}, nil
 }
 
